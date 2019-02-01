255c782bd06748642b086f553bc4fd7d1dbbe896
Perego Paolo <paolo.perego@gruppomol.it>
2019-02-01 17:14:40 +0100

    License changed to GPLv3 Added Makefile

--- enc0re.c
+++ enc0re.c
@@ -1,6 +1,21 @@
 /*
  * enc0re.c - custom binary encoder just for fun and for OSCE preparation.
- * (c) 2019, Paolo Perego - paolo@armoredcode.com
+ *
+ * This file is part of the enc0re distribution
+ * (https://github.com/thesp0nge/enc0re).
+ * Copyright (C) 2019, Paolo Perego - paolo@armoredcode.com
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * # Changelog
  * 
